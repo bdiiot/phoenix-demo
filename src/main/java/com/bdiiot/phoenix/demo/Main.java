79d5276bcae9e2fbcc20cfb788b55c16f84eb098
Eber H <eber.h@outlook.com>
2019-11-18 19:18:54 +0800

    test phoenix

--- src/main/java/com/bdiiot/phoenix/demo/Main.java
+++ src/main/java/com/bdiiot/phoenix/demo/Main.java
@@ -4,16 +4,16 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        String jdbc_url = "jdbc:phoenix:bigdata.t01.58btc.com,bigdata.t02.58btc.com,bigdata.t03.58btc.com:2181:/hbase-secure:hbase-bd@58BTC.COM:/etc/security/keytabs/hbase.headless.keytab";
+        String jdbc_url = "jdbc:phoenix:h11.bdiiot.com,h12.bdiiot.com,h13.bdiiot.com:2181:/hbase-secure:hbase-bdiiot@BDIIOT.COM:/etc/security/keytabs/hbase.headless.keytab";
         Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
         Connection connection = DriverManager.getConnection(jdbc_url);
         Statement statement = connection.createStatement();
 
-        String sql = "upsert into test.test_hbase values (1,'a','2019','2019')";
+        String sql = "upsert into test.test_phoenix values (1,'a')";
         System.out.println(statement.executeUpdate(sql));
         connection.commit();
 
-        sql = "select * from test.test_hbase";
+        sql = "select * from test.test_phoenix";
         ResultSet resultSet = statement.executeQuery(sql);
         while (resultSet.next()) {
             System.out.println(resultSet.getObject(1).toString());
